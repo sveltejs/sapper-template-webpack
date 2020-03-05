dc9446c2bf77a3daf89eec66c1cf7677ab3a4c22
Doug A <douganderson444@gmail.com>
2020-03-05 09:18:31 -0500

    hotReload:true
    issue 2377 closed, let's go hot!

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
 						options: {
 							dev,
 							hydratable: true,
-							hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
+							hotReload: true
 						}
 					}
 				}
